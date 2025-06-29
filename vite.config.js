850fb90f98375e4ad6825b656fc5bddfb63f9e8d
Mark Villudo <markvilludo@Marks-MacBook-Pro.local>
2025-06-29 10:56:28 +0800

    chore: deployment

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,13 @@ import path from "path";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/assets/js/jquery-3.7.1.min.js',
+                'resources/assets/js/bootstrap/bootstrap.bundle.min.js',
+                'resources/assets/js/bootstrap/main.js'
+            ],
             refresh: true,
         }),
         //additionals
